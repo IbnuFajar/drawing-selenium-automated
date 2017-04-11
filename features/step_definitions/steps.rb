f7255287f81206a8fe46ffcc2ced9cc5d88aae2c
Ibnu Yunardi <ibnu.fy@gmail.com>
2017-04-11 18:30:30 +0700

    First test

---
+++ features/step_definitions/steps.rb
@@ -0,0 +1,24 @@
+require 'selenium-webdriver'
+require 'rubygems'
+
+driver = Selenium::WebDriver.for :firefox
+
+Given(/^I Open sketchtoy$/) do
+  driver.get "http://sketchtoy.com/"
+  sleep(5)
+end
+
+Then(/^I Want to drawing$/) do
+  driver.find_element(:xpath, "html/body/div[1]/div[5]/div[2]/canvas").click
+  element = driver.find_element(:xpath, "html/body/div[1]/div[5]/div[2]/canvas");
+  driver.action.move_to(element).perform
+  driver.action.click_and_hold(element).perform
+  driver.action.move_by(150, 50).click.perform
+  driver.action.move_to(element).perform
+  driver.action.click_and_hold(element).perform
+  driver.action.move_by(100, 50).click.perform
+  driver.action.move_to(element).perform
+  driver.action.click_and_hold(element).perform
+  driver.action.move_by(300, 10).click.perform
+  sleep (5)
+end
---
+++ features/test.feature
@@ -0,0 +1,6 @@
+Feature: Contoh
+  In order to value
+  As a role
+Scenario: Drawing
+  Given I Open sketchtoy
+  Then I Want to drawing
